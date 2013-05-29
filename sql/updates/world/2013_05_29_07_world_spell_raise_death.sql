e67b8e11094c682f4173cb3620e636834e276fc3
FireEmerald <Chris.Merkel@web.de>
2013-05-29 23:20:53 +0200

    Changelog, Version.

--- sql/updates/world/2013_05_29_07_world_spell_raise_death.sql
+++ sql/updates/world/2013_05_29_07_world_spell_raise_death.sql
@@ -2,3 +2,5 @@
 DELETE FROM `spell_script_names` WHERE `spell_id` = -46584;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (-46584, "spell_dk_raise_dead");
+
+UPDATE `version` SET `db_version` = 'EDB 1.038' WHERE `core_version` = 'EchoCore';
