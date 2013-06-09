805f8098fa9acbce4b157975fa7975b6f1f3a794
FireEmerald <Chris.Merkel@web.de>
2013-06-09 13:20:26 +0200

    DB/Creature: Update: Only show Blix Fixwidget during Brewfest (Thx fedehacker)

---
+++ sql/updates/world/2013_06_09_01_world_creature_brewfest.sql
@@ -0,0 +1,4 @@
+-- DB/Creature: Update: Only show Blix Fixwidget during Brewfest | by fedehacker
+
+DELETE FROM `game_event_creature` WHERE `guid` = 152120 AND `eventEntry` = 24;
+INSERT INTO `game_event_creature` (`guid`, `eventEntry`) VALUES (152120,24);
